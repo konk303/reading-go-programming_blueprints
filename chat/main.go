1d9fde23d14b2e0b4006383976b16016561c373f
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2022-01-13 18:17:08 +0900

    Use tracer

--- chat/main.go
+++ chat/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"goblueprints/trace"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
@@ -26,6 +28,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します”
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始します
